a76207c483dc2992deaee902927daaec289a938e
hank0438 <nthu107062532@gapp.nthu.edu.tw>
2019-03-24 16:32:32 +0800

    correct comment aaa

--- test1.c
+++ test1.c
@@ -1,10 +1,25 @@
-int main(){ //hihihi//
-	/*science*/
-	1.23E4
-	1.23E+4
-	01.23E-4
-	123E04
-	"01.23E-4"
-	printf("aabb\n\tcc");
-	return 0;
+// Sample input
+/*
+ * This is a multiple lines comment.
+ */
+
+#pragma source on
+#pragma statistic on
+#pragma token on
+
+int main(void) {
+    int a;
+    float b = -0.1;
+    float c = -0032.800E-4;
+
+    if (c == a + b) {
+        c = 000.0001;
+    }
+    else {
+        c = 0001.1000;
+    }
+
+    print "aa\"bb\\cc\n\t\""
+
+    return 0;
 }
